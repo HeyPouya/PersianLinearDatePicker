ab1ec3da99896f6a448e53133aa8b279b04a4ddb
Pouya <pouyaheydary@gmail.com>
2020-02-05 01:17:20 +0330

    added show selected time button to the sample app

--- app/src/main/java/com/pouyaheydari/persianlineardatepicker/MainActivity.kt
+++ app/src/main/java/com/pouyaheydari/persianlineardatepicker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pouyaheydari.persianlineardatepicker
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +13,10 @@ class MainActivity : AppCompatActivity() {
 
         btnFixTime.setOnClickListener {
             datePicker.setDate(1321, 7, 30)
+        }
+
+        btnGetTime.setOnClickListener {
+            Toast.makeText(this, datePicker.getFormattedDate(), Toast.LENGTH_LONG).show()
         }
     }
 }
